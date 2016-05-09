b7ad1f34720397e2e687333666b901eb632fcca2
Phillip Webb <pwebb@pivotal.io>
2016-05-09 15:17:27 -0700

    Use mock VehicleDetailsService on integration test
    Update `ApplicationIntegrationTest` with a mock `VehicleDetailsService`
    bean so that a running service is no longer needed.

--- src/test/java/sample/ApplicationIntegrationTest.java
+++ src/test/java/sample/ApplicationIntegrationTest.java
@@ -16,18 +16,24 @@
 
 package sample;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import sample.domain.VehicleIdentificationNumber;
+import sample.service.VehicleDetails;
+import sample.service.VehicleDetailsService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 /**
  * Integration test for {@link Application} starting on a random port.
@@ -39,8 +45,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 @AutoConfigureTestDatabase
 public class ApplicationIntegrationTest {
 
+	private static final VehicleIdentificationNumber VIN = new VehicleIdentificationNumber(
+			"01234567890123456");
+
 	@Autowired
 	private TestRestTemplate restTemplate;
+
+	@MockBean
+	private VehicleDetailsService vehicleDetailsService;
+
+	@Before
+	public void setup() {
+		given(this.vehicleDetailsService.getVehicleDetails(VIN))
+				.willReturn(new VehicleDetails("Honda", "Civic"));
+	}
 
 	@Test
 	public void test() {
