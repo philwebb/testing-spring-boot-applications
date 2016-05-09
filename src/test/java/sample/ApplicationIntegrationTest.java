31e92fc94698d463267b495a383d943be27c299d
Phillip Webb <pwebb@pivotal.io>
2016-05-09 15:17:04 -0700

    Use @AutoConfigureTestDatabase on integration test
    Update `ApplicationIntegrationTest` to use `@AutoConfigureTestDatabase`
    so that a real database is no longer required.

--- src/test/java/sample/ApplicationIntegrationTest.java
+++ src/test/java/sample/ApplicationIntegrationTest.java
@@ -20,6 +20,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -35,6 +36,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestDatabase
 public class ApplicationIntegrationTest {
 
 	@Autowired
